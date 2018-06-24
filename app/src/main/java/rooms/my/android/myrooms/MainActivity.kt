a25c3f2b98e6de1b9b72db526e9dc19b2e662434
Mateus Andrade <mateusdoandrade@gmail.com>
2018-06-24 20:44:32 -0300

    Initial commit

---
+++ app/src/main/java/rooms/my/android/myrooms/MainActivity.kt
@@ -0,0 +1,12 @@
+package rooms.my.android.myrooms
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
